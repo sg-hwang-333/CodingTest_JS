b643bd868c714b1af27508416dfaff42e2a1af40
Seulgi Hwang <hsg151515@naver.com>
2023-12-28 20:12:31 +0900

    [level unrated] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,12 @@
+function solution(n, slicer, num_list) {
+    let a = slicer[0];
+    let b = slicer[1];
+    let c = slicer[2];
+    
+    switch (n) {
+        case 1: return num_list.slice(0, b + 1); 
+        case 2: return num_list.slice(a, num_list.length);
+        case 3: return num_list.slice(a, b + 1);
+        case 4: return num_list.slice(a, b + 1).filter((_, i) => i % c === 0);
+    }
+}
