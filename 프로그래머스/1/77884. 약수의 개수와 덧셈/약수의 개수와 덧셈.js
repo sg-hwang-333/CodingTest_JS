ba81d4670df4dc33f6eff6268595838f0cc4ef72
Seulgi Hwang <hsg151515@naver.com>
2024-03-13 09:32:07 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.51 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,15 @@
+function solution(left, right) {
+    var answer = 0;
+    
+    for (let i = left; i <= right; i++) {
+        let n = 0;
+        
+        for (let j = 1; j <= i; j++){
+            if (i % j == 0) n++;
+        }
+        
+        if (n % 2 == 0) answer += i;
+        else answer -= i;
+    }
+    return answer;
+}
