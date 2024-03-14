24aad0db9107b9f37a8cdcc2c8469690951d59ef
Seulgi Hwang <hsg151515@naver.com>
2024-03-14 09:29:10 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,9 @@
+function solution(price, money, count) {
+    var answer = 0;
+    
+    for (let i = 1; i <= count; i++) {
+        answer += i * price;
+    }
+    
+    return answer - money > 0 ? answer - money : 0;
+}
